c7c89b591255cd2f6aa6f86a2b0e4796fe4f825a
Pavel Chernyshev <titan100695@yandex.ru>
2021-01-25 19:47:07 +0300

    initial

---
+++ loops_arrays_structs_enums/loops_arrays_structs_enums/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  loops_arrays_structs_enums
+//
+//  Created by Павел Чернышев on 25.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
