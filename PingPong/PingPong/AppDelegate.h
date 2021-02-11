93374a57d04d24246afff8c711e1a1e14b6e39ed
Pavel Chernyshev <titan100695@yandex.ru>
2021-02-11 20:58:54 +0300

    inin ping pong game app

---
+++ PingPong/PingPong/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by Павел Чернышев on 11.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PingPong/PingPong/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PingPong
+//
+//  Created by Павел Чернышев on 11.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
